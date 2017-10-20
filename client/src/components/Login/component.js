f4042c8e1862ad4bb7ed98495e0b4f5a650da9df
imoralescs <israel@savvycard.com>
2017-10-19 23:44:21 -0400

    jwt

--- client/src/components/Login/component.js
+++ client/src/components/Login/component.js
@@ -8,17 +8,17 @@ export default function Dashboard(props) {
 		);
 	}
 	return (
-		<div className="container">
-			<form className="login" onSubmit={props.submitForm}>
+		<div>
+			<form className="login-form" onSubmit={props.submitForm}>
 				<div className="input-group">
-					<label>Username</label>
-					<input type="text" placeholder="Username" value={props.username} onChange={props.handleUsernameChanged} autoComplete="off"/>
+					<label className="login-form__label">Username</label>
+					<input className="login-form__input" type="text" placeholder="Username" value={props.username} onChange={props.handleUsernameChanged} autoComplete="off"/>
 				</div>
 				<div className="input-group">
-					<label>Password</label>
-					<input type="password" placeholder="Password" value={props.password} onChange={props.handlePasswordChanged} autoComplete="off"/>
+					<label className="login-form__label">Password</label>
+					<input className="login-form__input" type="password" placeholder="Password" value={props.password} onChange={props.handlePasswordChanged} autoComplete="off"/>
 				</div>
-				<input type="submit" value="Login" role="button"/>
+				<input className="login-form__submit" type="submit" value="Login" role="button"/>
 			</form>
 		</div>
 	);
